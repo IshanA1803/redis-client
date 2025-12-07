f3c8a3a0dd5f1ced860f49723e6d57d361a38531
Ishan Aryan <singhishan67@gmail.com>
2025-12-07 08:58:31 +0000

    Add RedisClient.h header file
    Defined RedisClient interface that manages connection to a Redis server.

---
+++ Client/RedisClient.h
@@ -0,0 +1,24 @@
+#ifndef REDIS_CLIENT_H
+#define REDIS_CLIENT_H
+
+#include <string>
+#include <iostream>
+#include <netdb.h>
+#include <unistd.h>
+#include <cstring>
+#include <sys/socket.h>
+
+class RedisClient {
+public:
+    RedisClient(const std::string& host, int port);
+    ~RedisClient();
+
+    bool connectToServer();
+    void disconnect();
+
+private:
+    std::string host;
+    int port;
+    int sockfd; 
+};
+#endif // REDIS_CLIENT_H
