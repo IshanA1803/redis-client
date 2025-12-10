d7c89ce0f3d579c7ec2d2e81999e635da0284eb0
Ishan Aryan <singhishan67@gmail.com>
2025-12-10 17:51:58 +0000

    Define CommandHandler interface
    Added CommandHandler class header and declared static splitArgs() utility
    function for tokenizing CLI input. No implementation yet.

---
+++ Client/CommandHandler.h
@@ -0,0 +1,13 @@
+#ifndef COMMAND_HANDLER_H
+#define COMMAND_HANDLER_H
+
+#include <vector>
+#include <string>
+
+class CommandHandler {
+public:
+    // Split commands into tokens
+    static std::vector<std::string> splitArgs(const std::string &input);
+};
+
+#endif // COMMAND_HANDLER_H
